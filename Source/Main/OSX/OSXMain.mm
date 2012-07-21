a0e13b4c43589545066b982407886ca8cd5012fd
joer <joer@sunsidegames.com>
2012-07-21 05:51:34 +0300

    Application launches and finds base folder automatically.
    Signed-off-by: joer <joer@sunsidegames.com>

---
+++ Source/Main/OSX/OSXMain.mm
@@ -0,0 +1,30 @@
+/*! \file OSXMain.cpp
+	\copyright Copyright (c) 2012 Sunside Inc., All Rights Reserved.
+	\copyright See Radiance/LICENSE for licensing terms.
+	\author Joe Riedel
+	\ingroup Main
+*/
+
+#include "../QtAppMain.h"
+#include <string.h>
+#include <unistd.h>
+#import <CoreFoundation/CoreFoundation.h>
+
+extern "C" int main(int argc, char *argv[]) {
+	
+#if defined(RAD_OPT_PC_TOOLS)
+	if (argc < 2 || strcmp(argv[1], "-nosetwd")) {
+		// Set working directory to the same folder that our .app bundle is in.
+		char bundlepath[1024];
+		CFURLRef url = CFBundleCopyBundleURL(CFBundleGetMainBundle());
+		CFURLRef url2 = CFURLCreateCopyDeletingLastPathComponent(0, url);
+		if (CFURLGetFileSystemRepresentation(url2, 1, (UInt8 *)bundlepath, 1024)) {
+			chdir(bundlepath);
+		}
+		CFRelease(url);
+		CFRelease(url2);
+	}
+#endif
+	
+	return QtAppMain(argc, (const char **)argv);
+}
