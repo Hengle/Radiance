83ad16c315945ec0bd681e798e5fd4c7a5ab0c0b
joer <joer@sunsidegames.com>
2012-07-31 16:46:25 +0300

    Replace a string loop with .replace()
    Signed-off-by: joer <joer@sunsidegames.com>

--- Source/Engine/Tools/Editor/ContentBrowser/EditorContentTexture.cpp
+++ Source/Engine/Tools/Editor/ContentBrowser/EditorContentTexture.cpp
@@ -141,11 +141,7 @@ RADENG_API pkg::IdVec RADENG_CALL CreateTextures(QWidget *parent, const pkg::Pac
 		if (!file::ExpandToNativePath(nativePrefix.toAscii().constData(), native, file::MaxFilePathLen+1))
 			return ids;
 		nativePrefix = native;
-		for (int i = 0; i < nativePrefix.length(); ++i)
-		{
-			if (nativePrefix[i] == '\\')
-				nativePrefix[i] = '/';
-		}
+		nativePrefix.replace('\\', '/');
 	}
 
 	fd.setDirectory(nativePrefix + QString("/Textures"));
