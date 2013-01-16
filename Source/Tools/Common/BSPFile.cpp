922d5a4abd036bc1ee73df37e620ebf7c2e36501
joer <joer@sunsidegames.com>
2013-01-16 00:49:10 -0700

    Removed dead files. Relocated max scripts.
    Signed-off-by: joer <joer@sunsidegames.com>

--- Source/Tools/Common/BSPFile.cpp
+++
@@ -1,6 +0,0 @@
-// BSPFile.cpp
-// Copyright (c) 2009 Pyramind Labs LLC, All Rights Reserved
-
-// work around scons not being able to include thing outside of the SConstruct directory.
-
-#include "../../Common/BSPFile.cpp"
